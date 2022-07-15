f505a21d7928b8e9281e1b55b0368bbe428d2307
keonjukes <keonofthephanerozoiceon@gmail.com>
2022-07-15 16:28:58 -0400

    Dad jokes favicon and app setup and working

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <h1>Dad Jokes!</h1>
+      <p>I have some jokes that are bound to make you laugh!</p>
     </div>
   );
 }
